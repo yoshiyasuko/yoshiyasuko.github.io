9d8e7551aa697716bde92a7d00555daabe38400b
Yoshiyasu <te108128@gmail.com>
2020-04-30 19:01:55 +0900

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/shinchan-hohoi.jpg') {
+        myImage.setAttribute('src', 'images/kazzwatabe.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/shinchan-hohoi.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeader = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('あなたの名前を入力してください。');
+    if (!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeader.textContent = 'しんちゃんはすごいよ、' + myName;
+    }
+}
+
+if (!(localStorage.getItem('name'))) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeader.textContent = 'しんちゃんはすごいよ、' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
